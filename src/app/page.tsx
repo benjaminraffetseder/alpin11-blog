75e0372053a6d69595c7400c40d3e4e938f59059
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2023-06-26 16:31:05 +0200

    added home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { Box, Container, Heading } from "@/components/client/chakra-ui";
+import { siteConfig } from "@/config/site";
+
 export default function Home() {
+  const { description } = siteConfig;
   return (
     <main>
-      <h1>Next.js + Chakra UI</h1>
+      <Box py={10}>
+        <Container>
+          <Box mb={10} maxW="65ch">
+            <Heading size="xl" color="black.50">
+              {description}
+            </Heading>
+          </Box>
+        </Container>
+      </Box>
     </main>
   );
 }
