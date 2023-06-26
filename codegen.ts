4e352cabf975ec08e0073ea8c42980b56dc4e3b1
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2023-06-26 16:26:02 +0200

    added graphql with codegen, added local fallback for gql api

---
+++ codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+dotenv.config();
+const config: CodegenConfig = {
+  schema: process.env.API,
+  documents: "src/graphql/**/*.graphql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-graphql-request",
+      ],
+      config: {
+        rawRequest: true,
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+  },
+};
+export default config;
