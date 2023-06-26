439325467dc8128ebf551c172f65b7c4dcf3fc80
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2023-06-26 18:47:16 +0200

    create page init

---
+++ src/app/create/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Container, Heading } from "@chakra-ui/react";
+
+const CreatePage = () => {
+  return (
+    <Container>
+      <Heading>Create Post</Heading>
+    </Container>
+  );
+};
+
+export default CreatePage;
